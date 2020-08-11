7645e71a65ba43161959eaaf45dde2affcf34f72
joh29 <joh29@uclive.ac.nz>
2020-08-11 20:30:36 +1200

    response

--- src/client.py
+++ src/client.py
@@ -1,11 +1,16 @@
 import socket
+import select
 import sys
 import re
 
 MIN_PORT = 1024     # Inclusive
 MAX_PORT = 64000    # Inclusive
-
 MAGIC_NUMBER = 0x497E
+DT_REQUEST_CODE = 0x0001
+DT_RESPONSE_CODE = 0x0001
+DATE_REQUEST_CODE = 0x0001
+TIME_REQUEST_CODE = 0x0001
+
 
 
 def check_num_parameters():
@@ -17,9 +22,13 @@ def check_num_parameters():
 
 
 def get_info_type_parameter():
-    """ Return the info type parameter (parameter index 1).
+    """ Get the info type parameter (parameter index 1).
+    Checks that it is either "date", or "time". if it's neither, an exception is thrown.
     Assumes the parameter exists.
-    Checks that it is either "date", or "time" - a exception is raised if it's not. """
+
+    Returns:
+          (string) type of data the user asked for (either, "date" or "time")
+    """
     info_type = sys.argv[1]
     if info_type not in ("date", "time"):
         raise Exception("""First parameter (info type) must be "date" or "time" """)
@@ -27,13 +36,15 @@ def get_info_type_parameter():
 
 
 def get_ip_parameter():
-    """ Return the ip parameter (parameter index 2).
+    """ Get the ip parameter (parameter index 2) in dotted decimal form (string).
     Assumes the parameter exists.
 
     If the parameter is given in dotted decimal notation, return this
+    If it's given as a textual addr (eg thing.that.nz), convert it to dotted decimal.
 
-    If it's given as a textual addr (eg thing.that.nz), convert it to dotted decimal with
-    getaddrinfo. """
+    Returns:
+         (string) ip address the user want's to connect to in dotted decimal.
+    """
     ip = sys.argv[2]
     if not is_in_dotted_decimal(ip):
         ip = get_dotted_decimal(ip)
@@ -42,7 +53,11 @@ def get_ip_parameter():
 
 def is_in_dotted_decimal(addr):
     """ Is the addr in dotted decimal?
-    in form "x1.x2.x3.x4" where x1 - x4 are integers between 0 and 255 (inclusive). """
+    in form "x1.x2.x3.x4" where x1 - x4 are integers between 0 and 255 (inclusive).
+
+    Returns:
+        (bool) wither the ip address is already in dotted decimal notation.
+    """
     match = re.search("([0-9]{1,3}\.){3}[0-9]{1,3}", addr)
     if match is None:
         return False   # Failed format check
@@ -54,7 +69,12 @@ def is_in_dotted_decimal(addr):
 
 
 def get_dotted_decimal(addr):
-    """ Given a address like thing.that.com - get it in dotted decimal """
+    """ Given a address like thing.that.com, get it in dotted decimal
+    uses socket.getaddrinfo with the addr and port parameter to obtain the ip address.
+
+    Returns:
+        (string): IPv4 address of addr (in dotted decimal)
+    """
     # return socket.getaddrinfo(addr, get_port_parameter())
     addr_info = socket.getaddrinfo(addr, get_port_parameter())
     ipv4_info = None
@@ -71,8 +91,12 @@ def get_dotted_decimal(addr):
 
 
 def get_port_parameter():
-    """ Return the port parameter (index 3).
-    Check it's an integer and that it's between MIN_PORT and MAX_PORT (inclusive) """
+    """ get the port parameter (index 3).
+    Check it's an integer and that it's between MIN_PORT and MAX_PORT (inclusive)
+
+    Returns:
+        (int) port user wants to connect to.
+    """
     port_str = sys.argv[3]
     try:
         port = int(port_str)
@@ -85,28 +109,74 @@ def get_port_parameter():
 
 
 def get_connected_socket(server_ip, port):
-    """ Get a new socket and connect it to server_ip and port. """
+    """ Get a new socket and connect it to server_ip and port.
+    Returns:
+        (socket) socket connected to server_ip and port
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)             # Create a socket object
     sock.connect((server_ip, port))
     return sock
 
 
-def make_packet(packetType, requestType):
-    """ Get a formatted packet with the packetType and requestType.
+def get_request_packet(request_type):
+    """ Get a formatted request packet with the packetType and requestType.
+
+    To be a 6 byte bytearray where
+      - 1st and 2nd bytes are the magic number
+      - 3rd and 4th bytes are the packet type, this will be request (1)
+      - 5th and 6th bytes are the request type. date = 1, time = 2
 
     Args:
-        packetType (String): The type of packet to send, either "date" or "time".
-        TODO finish this doc
+        request_type (string): type of request, either "date" or "time".
+
+    Returns:
+        (bytearray) the packet made
     """
+    if request_type == "date":
+        request_type_int = DATE_REQUEST_CODE
+    else:
+        request_type_int = TIME_REQUEST_CODE
+    pack_str = get_padded_bin_str(MAGIC_NUMBER, 16) + get_padded_bin_str(DT_REQUEST_CODE) + \
+               get_padded_bin_str(request_type_int)
+    return bytearray(pack_str, "utf-8")
+
+
+def get_padded_bin_str(n, num_chars=16):
+    """ Get the binary representation of the integer n padded with zeros at the beginning such
+    that the string has a length of num_chars.
+
+    Args:
+        n (int): number to convert.
+        num_chars (int): final string length
+
+    Returns:
+        (string) n as a binary string (num_chars long)
+    """
+    bin_n = bin(n)[2:]
+    to_add = ((num_chars - len(bin_n)) % num_chars)
+    return "0" * to_add + bin_n
 
 
 if __name__ == '__main__':
     check_num_parameters()
     ip = get_ip_parameter()
     port = get_port_parameter()
+    info_type = get_info_type_parameter()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     print("Connected to {} on port {}".format(ip, port))
 
-    packet = bytearray(input("msg: "), "utf-8")
-    sock.sendto(packet, (ip, port))
+    request_packet = get_request_packet(info_type)
+    sock.sendto(request_packet, (ip, port))
+
+    inputs = [sock]
+    outputs = []
+    message_queues = {}
+
+    while inputs:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+        for i in readable:
+            packet, source = i.recvfrom(1024)
+            print(packet)
+            quit()
--- src/server.py
+++ src/server.py
@@ -1,37 +1,16 @@
-import socket
 import sys
+import select
+import socket
 
 MIN_PORT = 1024     # Inclusive
 MAX_PORT = 64000    # Inclusive
+MAGIC_NUMBER = 0x497E
+DT_REQUEST_CODE = 0x0001
+DT_RESPONSE_CODE = 0x0001
+DATE_REQUEST_CODE = 0x0001
+TIME_REQUEST_CODE = 0x0001
 
 NUM_PORTS = 3
-
-MAGIC_NUMBER = 0x497E
-
-
-# s = socket.socket()         # Create a socket object
-# host = socket.gethostname() # Get local machine name
-# port = 12345                # Reserve a port for your service.
-# s.bind((host, port))        # Bind to the port
-#
-# print("Host = " + host)
-#
-# s.listen(5)                 # Now wait for client connection.
-#
-#
-# a = None
-# while a is None:
-#    a = s.accept()     # Establish connection with client.
-#
-# c, addr = a
-# print(c)
-# print(addr)
-#
-# while True:
-#    print('Got connection from', addr)
-#    text = input("Write something: ")
-#    c.send(bytes(text, 'utf-8'))
-# c.close()                # Close the connection
 
 
 def get_ports():
@@ -45,16 +24,17 @@ def get_ports():
             else:
                 raise Exception("Ports must be between {} and {} (inclusive)".format(MIN_PORT, MAX_PORT))
     else:
-        raise Exception("Server needs 3 ports!")
+        raise Exception("Server needs {} ports!".format(NUM_PORTS))
     return ports
 
 
 def get_socket(port):
     """ Get a new socket bound to this machine and port. """
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)             # Create a socket object
-    host = socket.gethostbyname(socket.gethostname())     # Get local machine name
-    s.bind((host, port))            # Bind to the port
-    return s
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = socket.gethostbyname(socket.gethostname())
+    sock.bind((host, port))
+    sock.setblocking(False)
+    return sock
 
 
 def get_sockets(ports):
@@ -62,28 +42,158 @@ def get_sockets(ports):
     return [get_socket(port) for port in ports]
 
 
-def get_connected_socket(sockets):
-    """ If one of the sockets has an incoming connection, accept it. """
+def handle_readable(readable):
+    """ Handle the incoming packets.
+    Given a list of readable sockets, read the data and process it accordingly
+    TODO what does that mean
+
+    Args:
+        readable (list): list of readable incoming things, given by select.select
+    """
+    for sock in readable:
+        if isinstance(sock, socket.socket):
+            packet, source = sock.recvfrom(1024)
+            process_packet(packet, sock, source)
+        else:
+            raise Exception("Got something that isn't a socket")
+
+
+def process_packet(packet, sock, source):
+    """ Given a packet, check if it's a request packet,
+    if the packet is a request, process it and send back the response.
+
+    Args:
+        packet (bytearray): The received packet
+        sock (socket): socket the packet was received from
+        source (tuple): TODO
+    """
+    magic_no, info_type, packet_type = get_request_packet_info(packet)
+
+    if not check_request(magic_no, info_type, packet_type):
+        # TODO Better messages pls
+        print("Uh oh, this isn't a valid request packet!")
+        return
+
+    print("Request received")
+    response_packet = compose_response_packet(info_type)
+    sock.sendto(response_packet, source)
+    print("Sending response")
+    quit()
+
+
+def compose_response_packet(request_type):
+    """ Compose a response packet in the order
+        magic number (2 bytes),
+        packet type (2 bytes),
+        language code (2 bytes),
+        year (2 bytes),
+        month (1 byte),
+        day (1 byte),
+        hour (1 byte),
+        minute (1 byte),
+        length (1 byte),
+        text ...
+    """
+
+    # TODO language code, time
+    year = 0
+    month = 0
+    day = 0
+    hour = 0
+    minute = 0
+    packet_str = get_padded_bin_str(MAGIC_NUMBER, 16) + get_padded_bin_str(DT_RESPONSE_CODE, 16) + \
+                 get_padded_bin_str(1, 16) + get_padded_bin_str(year, 16) + \
+                 get_padded_bin_str(month, 8) + get_padded_bin_str(day, 8) + \
+                 get_padded_bin_str(hour, 8) + get_padded_bin_str(minute, 8)
+    return bytearray(packet_str, "utf-8")
+
+
+def get_padded_bin_str(n, num_chars=16):
+    """ Get the binary representation of the integer n padded with zeros at the beginning such
+    that the string has a length of num_chars.
+
+    Args:
+        n (int): number to convert.
+        num_chars (int): final string length
+
+    Returns:
+        (string) n as a binary string (num_chars long)
+    """
+    bin_n = bin(n)[2:]
+    to_add = ((num_chars - len(bin_n)) % num_chars)
+    return "0" * to_add + bin_n
+
+
+def get_request_packet_info(packet):
+    """ Given a packet bytearray, extract the info from it.
+
+    If the packet is the right length to be a request (6 bytes), return its info,
+    If not, return None.
+
+    Args:
+        packet (bytearray): the received packet.
+
+    Returns:
+        (int) magic number from packet
+        (int) info type integer
+        (int) packet type integer
+    """
+    if len(packet) != 48:
+        return None
+
+    magic_no_str = packet[0:16]
+    magic_no = int(magic_no_str, base=2)
+
+    info_type_str = packet[16:32]
+    info_type = int(info_type_str, base=2)
+
+    packet_type_str = packet[32:48]
+    packet_type = int(packet_type_str, base=2)
+
+    return magic_no, info_type, packet_type
+
+
+def check_request(received_magic_no, packet_type, info_type):
+    """ Is the given packet a valid request?
+
+    is valid if:
+        received_magic_no is MAGIC_NUMBER,
+        packet_type = 1 (request)
+        info_type is "date" or "time"
+
+    Args:
+        received_magic_no (int): the magic number from the received packet
+        packet_type (int): the type of packet received (1 = request)
+        info_type (int): 1 = date, 2 = time
+
+    Returns:
+        (bool) whether the info passes the checks
+    """
+    if received_magic_no != MAGIC_NUMBER:
+        return False
+    elif packet_type != 1:
+        return False
+    elif info_type not in [1, 2]:
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
     port_numbers = get_ports()
     socket_ENG, socket_MAO, socket_GER = get_sockets(port_numbers)
 
-    # socket_ENG.listen()
-    # print(socket_ENG)
-    #
-    # connected_socket = None
-    # while connected_socket is None:
-    #     connected_socket = socket_ENG.accept()
-    #
-    # print(a)
-
     print("Server running on {}".format(socket_ENG.getsockname()[0]))
     print("ENG port: {}".format(socket_ENG.getsockname()[1]))
     print("MAO port: {}".format(socket_MAO.getsockname()[1]))
     print("GER port: {}".format(socket_GER.getsockname()[1]))
 
-    while True:
-        data, addr = socket_ENG.recvfrom(1024)
-        print(data, addr)
+    inputs = [socket_ENG, socket_MAO, socket_GER]
+    outputs = []
+    message_queues = {}
+
+    while inputs:
+        readable, writable, exceptional = select.select(inputs, outputs, inputs)
+        handle_readable(readable)
+
+
